0fa88fd23b9f0ab673a9ddf9785d19501cd94d64
Zira Cook <26914409+ziracook@users.noreply.github.com>
2021-01-16 23:36:49 -0800

    Toying around with colors

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, Menu, BrowserWindow, nativeTheme } = require('electron')
+
+const isMac = process.platform === 'darwin'
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -10,6 +12,8 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
+
+  nativeTheme.themeSource = 'dark'
 }
 
 app.whenReady().then(createWindow)
